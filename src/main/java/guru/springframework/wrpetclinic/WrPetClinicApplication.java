5cda52d06b9c4a12499d8173ac279ecb3599f0eb
Will Rowston <willrowston@gmail.com>
2019-11-19 15:36:58 -0500

    Initial Project add

---
+++ src/main/java/guru/springframework/wrpetclinic/WrPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.wrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/wrpetclinic/WrPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.wrpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
