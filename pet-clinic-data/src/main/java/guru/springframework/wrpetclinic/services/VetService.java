2a67033755c4b1e466f4a99e39a77af2eaf3ba3e
Will Rowston <willrowston@gmail.com>
2019-12-10 10:36:53 -0500

    implementing interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/wrpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.wrpetclinic.services;
+
+import guru.springframework.wrpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
