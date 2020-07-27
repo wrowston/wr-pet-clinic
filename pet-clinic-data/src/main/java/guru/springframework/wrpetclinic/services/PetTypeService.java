30ab1e54d15212b721f649c12559489eed9377bd
Will Rowston <willrowston@gmail.com>
2020-07-27 11:22:54 -0400

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/wrpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.wrpetclinic.services;
+
+import guru.springframework.wrpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
